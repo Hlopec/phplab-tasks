4fcbc5d56e87b9ee5b60d1e9e1f9259de13e4a98
Hlopec <vasylkruten@gmail.com>
2021-02-17 09:47:28 +0200

    added web tasks

--- src/web/functions.php
+++ src/web/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The $airports variable contains array of arrays of airports (see airports.php)
  * What can be put instead of placeholder so that function returns the unique first letter of each airport name
@@ -8,10 +9,17 @@
  *
  * @param  array  $airports
  * @return string[]
+ *
  */
 function getUniqueFirstLetters(array $airports)
 {
-    // put your logic here
-
-    return ['A', 'B', 'C'];
+    $first_letters = [];
+    foreach ($airports as $airport) {
+        $letter = substr($airport['name'], 0, 1);
+        if (!in_array($letter, $first_letters)) {
+            $first_letters[] = $letter;
+        }
+    }
+    sort($first_letters);
+    return $first_letters;
 }
